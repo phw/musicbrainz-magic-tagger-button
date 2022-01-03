1b1a393572d00e8d76a88c2bb3f55afd1f0cc70b
Philipp Wolfer <ph.wolfer@gmail.com>
2022-01-03 16:40:57 +0100

    Fixed search page include

--- mb-magic-tagger-button.user.js
+++ mb-magic-tagger-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          MusicBrainz Magic Tagger Button
 // @description   Automatically enable the green tagger button on MusicBrainz.org depending on whether Picard is running.
-// @version       0.7.1
+// @version       0.7.2
 // @author        Philipp Wolfer
 // @namespace     https://uploadedlobster.com
 // @license       MIT
@@ -13,7 +13,7 @@
 // @match         https://*.musicbrainz.org/release/*
 // @match         https://*.musicbrainz.org/series/*
 // @match         https://*.musicbrainz.org/taglookup
-// @include       /^https://*.musicbrainz.org/search/
+// @match         https://*.musicbrainz.org/search
 // @exclude       /^https://([.*].)?musicbrainz.org/release/add/
 // @exclude       /^https://([.*].)?musicbrainz.org/.*/create/
 // @exclude       /^https://([.*].)?musicbrainz.org/.*/.*/edit/
