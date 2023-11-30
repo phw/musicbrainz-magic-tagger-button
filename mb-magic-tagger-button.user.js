be73973e6818a56fbc55c501faf3d4a129e2dfe6
Philipp Wolfer <ph.wolfer@gmail.com>
2023-11-30 13:21:31 +0100

    Enabled script on artist releases page

--- mb-magic-tagger-button.user.js
+++ mb-magic-tagger-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          MusicBrainz Magic Tagger Button
 // @description   Automatically enable the green tagger button on MusicBrainz.org depending on whether Picard is running.
-// @version       0.7.7
+// @version       0.7.8
 // @author        Philipp Wolfer
 // @namespace     https://uploadedlobster.com
 // @license       MIT
@@ -14,6 +14,7 @@
 // @match         https://*.musicbrainz.org/series/*
 // @match         https://*.musicbrainz.org/taglookup
 // @match         https://*.musicbrainz.org/search
+// @match         https://*.musicbrainz.org/artist/*/releases
 // @exclude       /^https://(.*\.)?musicbrainz.org/release/add/
 // @exclude       /^https://(.*\.)?musicbrainz.org/.*/create/
 // @exclude       /^https://(.*\.)?musicbrainz.org/.*/.*/edit/
